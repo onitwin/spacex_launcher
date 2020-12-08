f74581bab422506936c63263766af3211563bf43
onitwin <onitwin@gmail.com>
2020-12-08 18:20:26 +0000

    added fetch function

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,4 +1,4 @@
-import React,{Fragment} from "react";
+import React,{Fragment,useState,useEffect} from "react";
 import List from "../components/List.js"
 import Sort from "../components/Sort.js"
 import Filter from "../components/Filter.js"
@@ -6,6 +6,18 @@ import Filter from "../components/Filter.js"
 
 //this will be the main container for all other elements
 const Homepage=()=>{
+
+  const [launchData,setLaunchData]=useState("")
+
+  function fetchLaunchData(){
+    fetch("https://api.spacexdata.com/v3/launches")
+    .then(data=>data.json())
+    .then(data=>setLaunchData(data))
+  }
+
+  useEffect(()=>{
+    fetchLaunchData()
+  },[])
 
 
 
