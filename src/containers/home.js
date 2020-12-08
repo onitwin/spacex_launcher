d1320218907d3e4e27baceb5c53db2c52cee326e
onitwin <onitwin@gmail.com>
2020-12-08 17:45:32 +0000

    component files added

--- src/containers/home.js
+++ src/containers/home.js
@@ -4,14 +4,15 @@ import Sort from "../components/Sort.js"
 import Filter from "../components/Filter.js"
 
 
-
-
+//this will be the main container for all other elements
 const Homepage=()=>{
+
+
 
 
   return(
     <Fragment>
-    <p>hello from the homepage</p>
+    <p alt="howdy">hello from the homepage</p>
     <List/>
     <Sort/>
     <Filter/>
