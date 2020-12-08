0808153134744076f140a5f8c44d91463b3d3e47
onitwin <onitwin@gmail.com>
2020-12-08 16:55:36 +0000

    basic files created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Homepage from "./containers/home.js"
 
-function App() {
+const  App=() =>{
   return (
     <div className="App">
   <h1>Hello from App.js</h1>
+  <Homepage/>
     </div>
   );
 }
---
+++ src/containers/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+const Homepage=()=>{
+
+
+  return(
+    <p>hello from the homepage</p>
+  )
+
+}
+export default Homepage
