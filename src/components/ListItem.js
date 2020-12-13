2de744a775471d7fc85e573fa9bab3581392abb2
onitwin <onitwin@gmail.com>
2020-12-13 22:55:32 +0000

    added unix converter functions

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React ,{useEffect,useState}from "react";
 
 
 //component to return individal launch item for display within list component
 const ListItem=({launch})=>{
+
+  const workingDate=launch.launch_date_unix*1000
+
+  const myDate=(item)=>{
+    let suffix=""
+    const dayOfMonth=new Date(item).getDate()
+    const digits=dayOfMonth.toString().split('')
+    if(digits[digits.length -1]==1){
+      suffix="st"
+    }else if(digits[digits.length-1]==2){
+      suffix="nd"
+    }else if(digits[digits.length-1]==3){
+      suffix="rd"
+    }else{
+      suffix="th"
+    }
+      return dayOfMonth.toString()+suffix
+  }
+
+  const myMonth=(item)=>{
+    const months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    const monthOfYear=new Date(item).getMonth()
+    return months[monthOfYear]
+  }
+
+  const myYear=(item)=>{
+    const year=new Date(item).getFullYear()
+    return year
+  }
+
+  const trueDate=`${myDate(workingDate)} ${myMonth(workingDate)} ${myYear(workingDate)}`
+
+
+
+
+
+
+
+
 
 
   return(
@@ -14,11 +53,14 @@ const ListItem=({launch})=>{
     <h1 className="mission-name-text">{launch.mission_name}</h1>
     </div>
     <div className="date-container">
-    <p className="flight-date">Date Placeholder</p>
+    <p className="flight-date">{trueDate}</p>
     <h3 className="flight-name">{launch.rocket.rocket_name}</h3>
     </div>
+
     </div>
   )
 
 }
 export default ListItem
+
+// <btn onClick={()=>myDate(launch)}>Convert</btn>
